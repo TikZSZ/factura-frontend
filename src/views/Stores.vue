bdbf81bddb25c4ed49d8ebd7e5cd1300209b471e
TikZSZ <repackergame@gmail.com>
2021-12-10 11:10:56 +0530

    initial commit

--- src/views/Stores.vue
+++ src/views/Stores.vue
@@ -34,9 +34,9 @@ const d = (async () => {
 Prisma
 <template>
 	<div class="mx-auto w-full">
-		<div v-if="!stores" class="bg-white min-h-screen dark:bg-gray-800 mt-44">
+		<div v-if="!stores" class="bg-white min-h-screen dark:bg-gray-800">
 			<div
-				class="lg:flex lg:items-center lg:justify-between w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20"
+				class="lg:flex lg:items-center md:mt-20 lg:justify-between w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20"
 			>
 				<h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
 					<span class="block">Want to sell ?</span>
