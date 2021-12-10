474a95885383bcfb8a45892e9271ce3ebf766751
TikZSZ <repackergame@gmail.com>
2021-12-10 09:15:32 +0530

    initial commit

--- src/misc/api.ts
+++ src/misc/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
-
+console.log(process.env);
 export default axios.create({
   baseURL:process.env.VUE_APP_BASE_URL || 'http://localhost:5000',
   withCredentials:true,
 })
+
+
