786baabc63b5d4eeb828f96183cdcc668b146982
TikZSZ <repackergame@gmail.com>
2021-12-12 11:58:52 +0530

    add NProgressbar

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import "./assets/base.css"
 import { createPinia } from 'pinia'
+import NProgress from './misc/NProgress'
+import "nprogress/nprogress.css"
 
 const app = createApp(App)
 
 const main = async ()=>{
   app.use(router)
   app.use(createPinia())
+  NProgress(router)
   const {useStore} = await import("@/store")
   const d =await new Promise<void>(async (resolve, reject) =>{
     await useStore().checkAuth()
--- src/misc/api.ts
+++ src/misc/api.ts
@@ -1,6 +1,27 @@
 import axios from "axios";
-export default axios.create({
+import { start, done } from "nprogress";
+const api =  axios.create({
   baseURL:import.meta.env['VITE_APP_BASEURL'] as string || 'http://localhost:5000' ,
   withCredentials:true,
 })
 
+
+api.interceptors.request.use(
+  (config) => {
+    start()
+    return config
+  },
+  (error) => {
+    done()
+    return Promise.reject(error)
+  }
+);
+
+api.interceptors.response.use(
+  (config) => {
+    done()
+    return config
+  }
+);
+
+export default api;
